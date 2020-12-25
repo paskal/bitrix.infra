71257be97fef17604b12eee37a5e0da37b4afdd5
Dmitry Verkhoturov <paskal.07@gmail.com>
2020-12-25 16:50:32 +0300

    shell lint, clarify readability

--- scripts/renew-dev.sh
+++ scripts/renew-dev.sh
@@ -21,40 +21,41 @@ mysql_config_file=$(
     m4 -D template="./private/percona-data/deleteme_XXXXXX"
 ) || exit
 
+mysql_binary_path = "docker exec -u0 percona-server /bin"
 mysql_config_inside_container="/var/lib/mysql/${mysql_config_file##*/}"
 
-echo "[client]\nuser = root\npassword = ${MYSQL_ROOT_PASSWORD}" > ${mysql_config_file}
+echo -e "[client]\nuser = root\npassword = ${MYSQL_ROOT_PASSWORD}" >${mysql_config_file}
 
-docker exec -u0 percona-server /bin/mysql --defaults-extra-file=${mysql_config_inside_container} -e "drop database if exists ${DEV_DB};"
-docker exec -u0 percona-server /bin/mysql --defaults-extra-file=${mysql_config_inside_container} -e "drop user if exists '${DEV_USER}'@'%';"
+${mysql_binary_path}/mysql --defaults-extra-file=${mysql_config_inside_container} -e "drop database if exists ${DEV_DB};"
+${mysql_binary_path}/mysql --defaults-extra-file=${mysql_config_inside_container} -e "drop user if exists '${DEV_USER}'@'%';"
 
 # prepare new dev database and user
-docker exec -u0 percona-server /bin/mysql --defaults-extra-file=${mysql_config_inside_container} -e "create database ${DEV_DB};"
-docker exec -u0 percona-server /bin/mysql --defaults-extra-file=${mysql_config_inside_container} -e "create user '${DEV_USER}'@'%' identified by '${DEV_PASSWORD}';"
-docker exec -u0 percona-server /bin/mysql --defaults-extra-file=${mysql_config_inside_container} -e "grant all on ${DEV_DB}.* to '${DEV_USER}'@'%';"
-docker exec -u0 percona-server /bin/mysql --defaults-extra-file=${mysql_config_inside_container} -e 'flush privileges;'
+${mysql_binary_path}/mysql --defaults-extra-file=${mysql_config_inside_container} -e "create database ${DEV_DB};"
+${mysql_binary_path}/mysql --defaults-extra-file=${mysql_config_inside_container} -e "create user '${DEV_USER}'@'%' identified by '${DEV_PASSWORD}';"
+${mysql_binary_path}/mysql --defaults-extra-file=${mysql_config_inside_container} -e "grant all on ${DEV_DB}.* to '${DEV_USER}'@'%';"
+${mysql_binary_path}/mysql --defaults-extra-file=${mysql_config_inside_container} -e 'flush privileges;'
 
 # create and load database dump
 # --no-tablespaces allows running not from root
 # --single-transaction will start a transaction before running
 # first --no-data run just dumps the schema for all tables,
 # second --ignore-table run ignores data from user sessions as we don't need to transfer it
-docker exec -u0 percona-server /bin/mysqldump --defaults-extra-file=${mysql_config_inside_container} --single-transaction --no-tablespaces --no-data ${PROD_DB} >prod-dump.sql
-docker exec -u0 percona-server /bin/mysqldump --defaults-extra-file=${mysql_config_inside_container} --single-transaction --no-tablespaces --ignore-table=${PROD_DB}.b_user_session ${PROD_DB} >>prod-dump.sql
-echo "[client]\nuser = ${DEV_USER}\npassword = ${DEV_PASSWORD}" > ${mysql_config_file}
+${mysql_binary_path}/mysqldump --defaults-extra-file=${mysql_config_inside_container} --single-transaction --no-tablespaces --no-data ${PROD_DB} >prod-dump.sql
+${mysql_binary_path}/mysqldump --defaults-extra-file=${mysql_config_inside_container} --single-transaction --no-tablespaces --ignore-table=${PROD_DB}.b_user_session ${PROD_DB} >>prod-dump.sql
+echo -e "[client]\nuser = ${DEV_USER}\npassword = ${DEV_PASSWORD}" >${mysql_config_file}
 cat prod-dump.sql | docker exec -u0 -i percona-server /bin/mysql --defaults-extra-file=${mysql_config_inside_container} ${DEV_DB}
 
 # change aspro and main site URL to reflect dev site value
-docker exec -u0 percona-server /bin/mysql --defaults-extra-file=${mysql_config_inside_container} -e "update b_iblock_element_property set VALUE = 'dev.favor-group.ru' where VALUE = 'favor-group.ru';" ${DEV_DB}
-docker exec -u0 percona-server /bin/mysql --defaults-extra-file=${mysql_config_inside_container} -e "update b_lang set SERVER_NAME = 'dev.favor-group.ru' where SERVER_NAME = 'favor-group.ru';" ${DEV_DB}
+${mysql_binary_path}/mysql --defaults-extra-file=${mysql_config_inside_container} -e "update b_iblock_element_property set VALUE = 'dev.favor-group.ru' where VALUE = 'favor-group.ru';" ${DEV_DB}
+${mysql_binary_path}/mysql --defaults-extra-file=${mysql_config_inside_container} -e "update b_lang set SERVER_NAME = 'dev.favor-group.ru' where SERVER_NAME = 'favor-group.ru';" ${DEV_DB}
 # change security filter settings to use new domain
-docker exec -u0 percona-server /bin/mysql --defaults-extra-file=${mysql_config_inside_container} -e "update b_option set VALUE = 'dev.favor-group.ru' where VALUE = 'favor-group.ru';" ${DEV_DB}
-docker exec -u0 percona-server /bin/mysql --defaults-extra-file=${mysql_config_inside_container} -e "update b_option set VALUE = 'dev.favor-group.ru' where MODULE_ID = 'security' and NAME = 'restriction_hosts_hosts';" ${DEV_DB}
-docker exec -u0 percona-server /bin/mysql --defaults-extra-file=${mysql_config_inside_container} -e "update b_option set VALUE = 'a:1:{s:4:\"host\";s:26:\"https://dev.favor-group.ru\";}' where MODULE_ID = 'security' AND name = 'restriction_hosts_action_options';" ${DEV_DB}
+${mysql_binary_path}/mysql --defaults-extra-file=${mysql_config_inside_container} -e "update b_option set VALUE = 'dev.favor-group.ru' where VALUE = 'favor-group.ru';" ${DEV_DB}
+${mysql_binary_path}/mysql --defaults-extra-file=${mysql_config_inside_container} -e "update b_option set VALUE = 'dev.favor-group.ru' where MODULE_ID = 'security' and NAME = 'restriction_hosts_hosts';" ${DEV_DB}
+${mysql_binary_path}/mysql --defaults-extra-file=${mysql_config_inside_container} -e "update b_option set VALUE = 'a:1:{s:4:\"host\";s:26:\"https://dev.favor-group.ru\";}' where MODULE_ID = 'security' AND name = 'restriction_hosts_action_options';" ${DEV_DB}
 # mark site as development one
-docker exec -u0 percona-server /bin/mysql --defaults-extra-file=${mysql_config_inside_container} -e "update b_option set VALUE = 'Y' where MODULE_ID = 'main' and NAME = 'update_devsrv';" ${DEV_DB}
+${mysql_binary_path}/mysql --defaults-extra-file=${mysql_config_inside_container} -e "update b_option set VALUE = 'Y' where MODULE_ID = 'main' and NAME = 'update_devsrv';" ${DEV_DB}
 # disable external access to the site
-docker exec -u0 percona-server /bin/mysql --defaults-extra-file=${mysql_config_inside_container} -e "update b_option set VALUE = 'Y' where MODULE_ID = 'main' and NAME = 'site_stopped';" ${DEV_DB}
+${mysql_binary_path}/mysql --defaults-extra-file=${mysql_config_inside_container} -e "update b_option set VALUE = 'Y' where MODULE_ID = 'main' and NAME = 'site_stopped';" ${DEV_DB}
 
 # copy files
 # --archive preserves file permissions and so on
@@ -83,9 +84,3 @@ rm -f prod-dump.sql
 
 # clean up tmp file with credentials
 rm -f -- "${mysql_config_file}"
-
-# TODO:
-# установка для разработки
-# закрыть публичную часть
-# поменять url
-
