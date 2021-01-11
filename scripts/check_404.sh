04731c6c8a667506366049a78f067e1801c445c6
Dmitry Verkhoturov <paskal.07@gmail.com>
2021-01-11 23:42:49 +0300

    add 404 check oneliner

---
+++ scripts/check_404.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+# this script prints current 404s from favor-group.ru for redirects troubleshooting
+# google shows year-old pages and we need to redirect poor souls clicking on them somewhere
+
+fgrep ' 404 ' logs/nginx/favor-group.ru.access.log | cut -d '"' -f 2| cut -d ' ' -f 2 | sort | uniq -c | less
