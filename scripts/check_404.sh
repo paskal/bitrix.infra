9dde9f98d2c147de13d0ce7d4dcbbaaefd94a1ec
Dmitry Verkhoturov <paskal.07@gmail.com>
2021-01-13 01:20:22 +0300

    fix bunch of 404s

--- scripts/check_404.sh
+++ scripts/check_404.sh
@@ -2,4 +2,4 @@
 # this script prints current 404s from favor-group.ru for redirects troubleshooting
 # google shows year-old pages and we need to redirect poor souls clicking on them somewhere
 
-fgrep ' 404 ' logs/nginx/favor-group.ru.access.log | cut -d '"' -f 2| cut -d ' ' -f 2 | sort | uniq -c | less
+fgrep '" 404 ' logs/nginx/favor-group.ru.access.log | cut -d '"' -f 2| cut -d ' ' -f 2 | sort | uniq -c | less
