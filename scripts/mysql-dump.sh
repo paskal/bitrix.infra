9868d5d401181891590795c3500b183fdb2cd6a2
Dmitry Verkhoturov <donich@bk.ru>
2021-02-24 23:28:14 +0000

    improve mysql dump script logging

--- scripts/mysql-dump.sh
+++ scripts/mysql-dump.sh
@@ -7,7 +7,8 @@ domain=favor-group.ru
 prod_db=$(echo ${domain} | tr '.' '_' | tr '-' '_')
 date="$(date +%Y-%m-%d)"
 time="$(date +%H-%M-%S)"
-backup_directory="/web/backup/$date"
+backup_directory_path="./backup/"
+backup_directory="$backup_directory_path$date"
 backup_file="$date-$time-$prod_db-mysqldump.sql.gz"
 
 if [ ! -d "$backup_directory" ]; then
@@ -34,7 +35,7 @@ mysql_config_inside_container="/var/lib/mysql/${mysql_config_file##*/}"
 # but will require -e flag in bash to work
 echo "[client]\nuser = root\npassword = ${MYSQL_ROOT_PASSWORD}" >${mysql_config_file}
 
-echo "Backing up MySQL to $backup_directory, $date $time"
+echo "Backing up MySQL to $backup_directory/$backup_file"
 
 ${mysql_binary_path}/mysqldump --defaults-extra-file=${mysql_config_inside_container} --routines --single-transaction --flush-logs --no-tablespaces --no-data "${prod_db}" | pigz -c >"$backup_directory/$backup_file"
 ${mysql_binary_path}/mysqldump --defaults-extra-file=${mysql_config_inside_container} --routines --single-transaction --flush-logs --no-tablespaces --ignore-table="${prod_db}".b_user_session "${prod_db}" | pigz -c >>"$backup_directory/$backup_file"
@@ -45,8 +46,7 @@ chown 1000:1000 "$backup_directory/$backup_file"
 rm -f -- "${mysql_config_file}"
 
 echo "Syncing backups to s3://$backup_s3_directory"
-
 # sync with S3 (Yandex in that case)
-HOME=/home/admin /usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 sync ./backup/ "s3://${backup_s3_directory}/"
+HOME=/home/admin /usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 sync "${backup_directory_path}" "s3://${backup_s3_directory}/"
 
 echo "Backup is complete"
