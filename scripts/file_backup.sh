96397777f2dc8b71b783d5d5bd72aff9d02bbddb
Dmitry Verkhoturov <paskal.07@gmail.com>
2021-04-06 01:50:34 +0200

    add duplicity file backup, run it in cron once a day

---
+++ scripts/file_backup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+set -e -u
+
+src="/web"
+dest="boto3+s3://favor-group-backup/duplicity_web_$(hostname)"
+
+# HOME is required to read .aws/credentials from it
+HOME="/home/admin" duplicity \
+  --no-encryption \
+  --full-if-older-than 60D \
+  --asynchronous-upload \
+  --s3-use-ia \
+  --s3-endpoint-url https://storage.yandexcloud.net \
+  --log-file /var/log/duplicity.log \
+  --archive-dir /root/.cache/duplicity \
+  --exclude '**/.git' \
+  --exclude '**/backup/' \
+  --exclude '**/logs/' \
+  --exclude '**/web/dev' \
+  "${src}" "${dest}"
