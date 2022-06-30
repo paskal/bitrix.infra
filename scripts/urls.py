3c088aabe491d30b0954aec03026d5e25d75a778
Dmitry Verkhoturov <paskal.07@gmail.com>
2022-06-30 02:52:54 +0200

    improve redirects script, simplify part of redirects

--- scripts/urls.py
+++ scripts/urls.py
@@ -5,8 +5,9 @@ from enum import Enum
 
 
 class RunTypes(Enum):
-    not_found = 'not_found'
     redirects = 'redirects'
+    not_found = 'simplify_redirects'
+    bad_status_codes = 'bad_status_codes'
 
     def __str__(self):
         return self.value
@@ -18,25 +19,56 @@ def remove_prefix(text: str, prefix: str) -> str:
     return text[len(prefix):] if text.rfind(prefix) == 0 else text
 
 
-def check_redirect(full_url: str, base_url: str):
-    """ Checks if the URL has non-200 status code and prints the redirect destination or the status code
-    """
+def retrieve_url(site: str, relative_url: str) -> requests.Response:
+    """Returns response for the provided URL,
+    or prints error message and returns nothing in case of infinite redirect."""
     try:
-        req = requests.get(full_url)
+        resp = requests.get(site + relative_url)
     except requests.exceptions.TooManyRedirects:
-        print(f"too many redirects on {remove_prefix(full_url, base_url)}")
+        print(f"too many redirects on {relative_url}")
         return
-    if remove_prefix(full_url, base_url) != remove_prefix(req.url, base_url):
-        print(f"redirect: {remove_prefix(full_url, base_url)}")
-    if req.status_code != 200:
-        print(f"code {req.status_code}: {remove_prefix(full_url, base_url)}")
+    return resp
+
+
+def check_redirect(resp: requests.Response, base_url: str, relative_url: str):
+    """Prints URL and status code of the provided response if it has non-200 status code,
+    or URL and it's redirect final destination or the status code in case it's not 301 or 302.
+    """
+    if relative_url != remove_prefix(resp.url, base_url):
+        print(f"redirect: {relative_url} -> {remove_prefix(resp.url, base_url)}")
+    bad_status_codes(resp, relative_url)
+
+
+def simplify_redirects(resp: requests.Response, base_url: str):
+    """Prints original redirect or the URL, and its final destination in case there is a chain of redirects.
+    That turns out to be useful to simplify redirects as well as for SEO, as original redirect might be to
+    non-indexed search page while the final destination might be SEO-friendly page,
+    but the search engine would never know."""
+    if len(resp.history) > 1:
+        print(f"{remove_prefix(resp.history[1].url, base_url)}"
+              f" -> {remove_prefix(resp.url, base_url)}")
+    bad_status_codes(resp, remove_prefix(resp.history[0].url, base_url))
+
+
+def bad_status_codes(resp: requests.Response, relative_url: str):
+    """Prints URL and status code of the provided response if it has non-200 and not 301 or 302 status code.
+    """
+    if resp.status_code != 200:
+        print(f"code {resp.status_code}: {relative_url}")
 
 
 def main(run_type: str, site: str, urls_file: str):
     for line in open(urls_file, 'r').readlines():
-        full_url = site + remove_prefix(line.strip(), site)
+        relative_url = remove_prefix(line.strip(), site)
+        resp = retrieve_url(site, relative_url)
+        if not resp:
+            continue
         if run_type == "redirects":
-            check_redirect(full_url, site)
+            check_redirect(resp, site, relative_url)
+        if run_type == "simplify_redirects":
+            simplify_redirects(resp, site)
+        if run_type == "bad_status_codes":
+            bad_status_codes(resp, site)
 
 
 if __name__ == '__main__':
@@ -53,8 +85,9 @@ if __name__ == '__main__':
         'run_type',
         type=RunTypes,
         choices=list(RunTypes),
-        help="Type of the run: either check for redirects and pages "
-             "with wrong status codes, or something which is not yet implemented."
+        help="Type of the run: either check for redirects and pages with wrong status codes,"
+             " or only print redirect chains which could be simplified,"
+             " or only pages which are not redirects with wrong status codes."
     )
     opts = parser.parse_args()
     main(str(opts.run_type), opts.site, opts.urls_file)
