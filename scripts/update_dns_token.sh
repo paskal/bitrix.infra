5f137f6040aa9d4433dc12674ca74e3c1a344a91
Dmitry Verkhoturov <paskal.07@gmail.com>
2022-07-25 02:05:53 +0000

    fix dnsrobocert work

--- scripts/update_dns_token.sh
+++ scripts/update_dns_token.sh
@@ -3,4 +3,4 @@ set -e -u
 
 # write down new token
 sed -i "s/.*\AUTH_KEY.*/\AUTH_KEY=$(yc iam create-token)/" "./private/environment/dnsrobocert.env"
-docker-compose up -d certbot
+docker compose up -d certbot
