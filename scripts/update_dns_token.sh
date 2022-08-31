da3fcf22f50ae8300bb2216a4ddf3929d2482f3f
Dmitry Verkhoturov <paskal.07@gmail.com>
2022-08-31 05:07:43 +0000

    fix renewing Yandex Cloud token not working due to lack of binary in PATH

--- scripts/update_dns_token.sh
+++ scripts/update_dns_token.sh
@@ -2,5 +2,5 @@
 set -e -u
 
 # write down new token
-sed -i "s/.*\AUTH_KEY.*/\AUTH_KEY=$(yc iam create-token)/" "./private/environment/dnsrobocert.env"
+sed -i "s/.*\AUTH_KEY.*/\AUTH_KEY=$($HOME/yandex-cloud/bin/yc iam create-token)/" "./private/environment/dnsrobocert.env"
 docker compose up -d certbot
