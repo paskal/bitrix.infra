52ccbaa32429769066aad880d2c0937c710a2d15
Dmitry Verkhoturov <paskal.07@gmail.com>
2022-11-11 22:02:42 +0000

    optimise only images in images and upload, and fix pngs if possible

--- scripts/optimise-images.sh
+++ scripts/optimise-images.sh
@@ -7,10 +7,10 @@ set -e -u
 sudo apt -y install optipng advancecomp jpegoptim
 
 echo "Optimising PNGs..."
-find web/prod -type f -iname "*.png" -exec optipng -o7 -preserve {} \;
-find web/prod -type f -iname "*.png" -exec advpng -z4 {} \;
+find web/prod/images web/prod/upload -type f -iname "*.png" -exec optipng -fix -o7 -preserve {} \;
+find web/prod/images web/prod/upload -type f -iname "*.png" -exec advpng -z4 {} \;
 
 echo "Optimising JPEGs..."
-find web/prod -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) -exec jpegoptim --strip-none {} \;
+find web/prod/images web/prod/upload -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) -exec jpegoptim --strip-none {} \;
 
 echo "Images optimisation complete"
