1df3b30247ef578564aa3557744aade820d6d87b
Dmitry Verkhoturov <paskal.07@gmail.com>
2022-11-11 21:41:33 +0100

    add setup.py and requirements.txt to urls.py script

---
+++ scripts/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='scripts',
+    version='',
+    packages=[''],
+    url='https://github.com/paskal/bitrix.infra',
+    license='MIT',
+    author='Dmitru Verkhoturov',
+    author_email='paskal.07@gmail.com',
+    description=''
+)
