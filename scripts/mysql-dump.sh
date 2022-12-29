a8900da10f1c036ed5fcc7652615d9af8f4842b1
Dmitry Verkhoturov <paskal.07@gmail.com>
2022-12-29 13:37:31 +0000

    fix backup ignore of .duplicity-cache directory

--- scripts/mysql-dump.sh
+++ scripts/mysql-dump.sh
@@ -48,6 +48,6 @@ rm -f -- "${mysql_config_file}"
 echo "Syncing backups to s3://$backup_s3_directory"
 # sync with S3 (Yandex in that case),
 # also ignore duplicity cache in the backup volume
-HOME=/home/admin /usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 sync "${backup_directory_path}" "s3://${backup_s3_directory}/mysql_$(hostname)/" --exclude .duplicity-cache
+HOME=/home/admin /usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 sync "${backup_directory_path}" "s3://${backup_s3_directory}/mysql_$(hostname)/" --exclude '.duplicity-cache/*'
 
 echo "Backup is complete"
