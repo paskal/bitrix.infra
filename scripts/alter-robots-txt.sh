c0c2cffad2d6ee0d04f9e6e587e34fbca140ef18
Dmitry Verkhoturov <paskal.07@gmail.com>
2023-02-25 16:43:02 +0100

    add separate robots.txt case for Tula, fix script description

--- scripts/alter-robots-txt.sh
+++ scripts/alter-robots-txt.sh
@@ -1,25 +1,33 @@
 #!/usr/bin/env sh
 set -e -u
 
-# This script blocks Google via robots.txt for regions other than
-# Moscow, Tula and SPB, so that they are indexed only in Yandex
+# This script blocks /info/blog/ via robots.txt for regions other than
+# Moscow so that it would be indexed only there.
+# Also, block /montag/ for Tula as it's not present there.
 
 echo "Getting list of robots.txt files..."
 robots_files=$(ls -1 web/*/aspro_regions/robots/*.txt)
 
+# perl replace commands adds text in the file only if it's not already there
+# \n doesn't work here for god knows why, so replaced it with \s
 for f in $robots_files; do
   filename=$(basename "${f}")
   case $filename in
   robots_favor-group.ru.txt)
     echo "Ignoring ${f}"
     ;;
+  robots_tula.favor-group.ru.txt)
+    echo "Processing ${f}"
+    # disallow blog and montag section
+    perl -0777 -pi -e 's/^(Disallow: \/bitrix\/\*)\s+(Allow)/\1\nDisallow: \/info\/blog\/\n\2/gm' "${f}"
+    perl -0777 -pi -e 's/^(Disallow: \/basket)\s+(Disallow: \/form\/)/\1\nDisallow: \/montag\/\n\2/gm' "${f}"
+    ;;
   *)
     echo "Processing ${f}"
-    # add text in the file only if it's not already there
-    # \n doesn't work here for god knows why, so replaced it with \s
-    perl -0777 -pi -e 's/^(Disallow: \/bitrix\/\*)\s+Allow/\1\nDisallow: \/info\/blog\/\nAllow/gm' "${f}"
+    # disallow only blog
+    perl -0777 -pi -e 's/^(Disallow: \/bitrix\/\*)\s+(Allow)/\1\nDisallow: \/info\/blog\/\n\2/gm' "${f}"
     ;;
   esac
 done
 
-echo "robots.txt files other than Moscow, have /info/blog/ disallow entry now"
+echo "robots.txt files other than Moscow, have /info/blog/ disallow entry now, and Tula disallows /montag/ also"
