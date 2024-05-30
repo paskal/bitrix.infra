da228c56c2516d88e2b4a2de4fc319b8b4cb31f6
Dmitry Verkhoturov <paskal.07@gmail.com>
2024-05-30 21:53:46 +0000

    add missing env file to zabbix agent mounts

--- scripts/update-dns-token.sh
+++ scripts/update-dns-token.sh
@@ -4,4 +4,4 @@ set -e -u
 # write down new token
 sed -i "s/.*\AUTH_KEY.*/\AUTH_KEY=$($HOME/yandex-cloud/bin/yc iam create-token)/" "./private/environment/dnsrobocert.env"
 # make certbot and zabbix-agent read new AUTH_KEY
-docker compose up -d certbot zabbix-agent
+docker compose up -d
