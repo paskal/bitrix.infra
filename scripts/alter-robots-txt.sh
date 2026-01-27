973c1191c5d99fd434ddd87f448e7d42b4b27414
Dmitry Verkhoturov <paskal.07@gmail.com>
2026-01-27 22:36:45 +0100

    chore: also block /projects/ for Tula in robots.txt script

--- scripts/alter-robots-txt.sh
+++ scripts/alter-robots-txt.sh
@@ -3,7 +3,7 @@ set -e -u
 
 # This script blocks /info/blog/ via robots.txt for regions other than
 # Moscow so that it would be indexed only there.
-# Also, block /montag/ for Tula as it's not present there.
+# Also, block /montag/ and /projects/ for Tula as they're not present there.
 
 echo "Getting list of robots.txt files..."
 robots_files=$(ls -1 web/*/aspro_regions/robots/*.txt)
@@ -18,9 +18,9 @@ for f in $robots_files; do
     ;;
   robots_tula.favor-group.ru.txt)
     echo "Processing ${f}"
-    # disallow blog and montag section
+    # disallow blog, montag, and projects sections
     perl -0777 -pi -e 's/^(Disallow: \/bitrix\/\*)\s+(Allow)/\1\nDisallow: \/info\/blog\/\n\2/gm' "${f}"
-    perl -0777 -pi -e 's/^(Disallow: \/basket)\s+(Disallow: \/form\/)/\1\nDisallow: \/montag\/\n\2/gm' "${f}"
+    perl -0777 -pi -e 's/^(Disallow: \/basket)\s+(Disallow: \/form\/)/\1\nDisallow: \/montag\/\nDisallow: \/projects\/\n\2/gm' "${f}"
     ;;
   *)
     echo "Processing ${f}"
@@ -30,4 +30,4 @@ for f in $robots_files; do
   esac
 done
 
-echo "robots.txt files other than Moscow, have /info/blog/ disallow entry now, and Tula disallows /montag/ also"
+echo "robots.txt files other than Moscow have /info/blog/ disallow entry now, and Tula disallows /montag/ and /projects/ also"
